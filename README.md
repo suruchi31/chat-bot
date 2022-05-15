414185604c75753805903381cf7da52a64b42e6c
suruchi <suruchibedi3108@gmail.com>
2022-05-15 11:39:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+download the zip file of this project from github.
+open project using vs code.
+in terminal, write 'npm run dev' or 'node server.js' 
+then run on the given port no. on browser or just write 'http://localhost:4044/'
+and stand sending the messages.   handle requires your name.
+use two web for better view of client and server.
---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server = app.listen(4044, function(){
+    console.log('listening for requests on port 4044,');
+});
+const cors = require('cors');
+app.use(cors({
+    origin: "*"
+}));
+// Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
